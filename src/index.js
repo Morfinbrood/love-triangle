6c2b432ecd517a4e6211778173197b61abbe884d
Yury Staravoitau <MorfinAll@mail.ru>
2018-02-24 05:20:58 +0300

    love-triangle/feature: Added final result. All tests passed.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  let offset_arr = preferences.slice("null");
+  offset_arr.unshift(0);
+  offset_arr.forEach(function (item, i, arr) {
+    if (i > 0) {
+      let spich1_love = item;
+      let spich2_love = arr[spich1_love];
+      let spich3_love = arr[spich2_love];
+      if (spich3_love === i && spich1_love !== spich2_love && spich2_love !== spich3_love) {
+        count++;
+      }
+    }
+  })
+  return count / 3;
 };
